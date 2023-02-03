59008a2d562c472d15c788013b8aa8cb3a1800f6
abdulloh5007 <abdulloh50007@gmail.com>
2023-02-03 18:22:53 +0500

    new

--- src/Components/Buyurtma/Buyurtma.jsx
+++ src/Components/Buyurtma/Buyurtma.jsx
@@ -3,8 +3,25 @@ import Container from '../Container/Container'
 import './Buyurtma.scss'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { message } from 'antd';
 
 function Buyurtma() {
+  window.addEventListener('click', (e) => {
+    console.log(e)
+  })
+
+  const [messageApi, contextHolder] = message.useMessage();
+  const success = (e) => {
+    e.preventDefault()
+
+    messageApi
+      .open({
+        type: 'loading',
+        content: 'Ma`lumot tekshirilmoqda..',
+        duration: 2.5,
+      })
+      .then(() => message.success('Ma`lumotlaringiz muvaffaqiyatlik jo`natildi', 2.5))
+  };
   return (
     <div>
       <Container>
@@ -12,16 +29,20 @@ function Buyurtma() {
           <div className="order-left">
             <h2>Buyurtma berish</h2>
             <p>Formani toʻldiring, mutaxascislardan bepul konsultatsiya oling va chegirmaga ega boling</p>
-            <TextField id="outlined-basic" label="Ismingiz" variant="outlined" />
-            <TextField
-              label="Raqamingiz"
-              id="outlined-start-adornment"
-              sx={{ m: 3, width: '42ch' }}
-              defaultValue="+998"
-            />
-            <Button variant="contained" color="success">
-              Buyurtma berish
-            </Button>
+            <form action="#" onSubmit={success}>
+              <TextField name='ism' id="outlined-basic" label="Ismingiz" variant="outlined" />
+              <TextField
+                name='raqam'
+                label="Raqamingiz"
+                id="outlined-start-adornment"
+                sx={{ m: 3, width: '42ch' }}
+                defaultValue="+998"
+              />
+              {contextHolder}
+              <Button type='submit' variant="contained" color="success">
+                Buyurtma berish
+              </Button>
+            </form>
           </div>
           <div className="order-right">
             <h2>Chegirma</h2>
