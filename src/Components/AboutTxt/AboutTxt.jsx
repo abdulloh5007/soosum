beb083f94978665589ccad3d7b9ae77d2ee20f73
abdulloh5007 <abdulloh50007@gmail.com>
2023-02-03 17:24:50 +0500

    new

--- src/Components/AboutTxt/AboutTxt.jsx
+++ src/Components/AboutTxt/AboutTxt.jsx
@@ -14,18 +14,18 @@ function AboutTxt() {
     <div>
         <Container>
             <div className="aboutTxt">
-                <div data-aos="fade-up-right" className="aboutTxt-first txt">
-                    <div>
+                <div className="aboutTxt-first txt">
+                    <div data-aos="fade-up-right">
                         <p>
                         Qizil jenshen bilan oddiy jenshenni farqi,yetishtirilishi.
 Jenshen mahsulotini ekishdan oldin ekin maydoni 2 yil og'itlar bilan sugorilib tayyorlanadi. 3-yili esa yerga jenshen 2 xil usulda ekiladi,1-jenshen niholini donalab ekish(koreada asosan shu usuldan foydalaniladi),2- urugni sepish orqali. Yerga jenshen ekilgandan song, 1 ~ 8 yil davomida jenshen turli og'itlar bilan sugorilib katta qilinadi.(jenshen yerda qancha turganiga qarab oshancha yillik jenshen deb nomlanadi). Yerdan yangi uzilgan 1~8 yillik ho'l jenshen(koreyschada insam인삼,susam수삼 deb nomlanadi)bu oddiy jenshen hisoblanadi(lekin buniham foydali hususiyatlari juda kop. Giyohlar ichida qizil jenshendan song birinchi orinda turadi)
                         </p>
                     </div>
-                    <img src={aboutTxtRight} alt="aboutTxtRight" />
+                    <img data-aos="fade-up-left" src={aboutTxtRight} alt="aboutTxtRight" />
                 </div>
-                <div data-aos="fade-up-left" className="aboutTxt-second txt">
-                    <img src={aboutTxtLeft} alt="aboutTxtLeft" />
-                    <div>
+                <div className="aboutTxt-second txt">
+                    <img data-aos="fade-up-right" src={aboutTxtLeft} alt="aboutTxtLeft" />
+                    <div data-aos="fade-up-left">
                         <p>
                         Qizil jenshen bilan oddiy jenshenni farqi,yetishtirilishi.
 Jenshen mahsulotini ekishdan oldin ekin maydoni 2 yil og'itlar bilan sugorilib tayyorlanadi. 3-yili esa yerga jenshen 2 xil usulda ekiladi,1-jenshen niholini donalab ekish(koreada asosan shu usuldan foydalaniladi),2- urugni sepish orqali. Yerga jenshen ekilgandan song, 1 ~ 8 yil davomida jenshen turli og'itlar bilan sugorilib katta qilinadi.(jenshen yerda qancha turganiga qarab oshancha yillik jenshen deb nomlanadi). Yerdan yangi uzilgan 1~8 yillik ho'l jenshen(koreyschada insam인삼,susam수삼 deb nomlanadi)bu oddiy jenshen hisoblanadi(lekin buniham foydali hususiyatlari juda kop. Giyohlar ichida qizil jenshendan song birinchi orinda turadi)
